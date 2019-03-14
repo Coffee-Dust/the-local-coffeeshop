7c19fde9775f4cd7f55ec4fdde9eaa251530ab6e
Jonathan Dust <d24dust@hotmail.com>
2019-03-14 14:13:08 -0500

    add main model relationships

--- app/models/coffeeshop.rb
+++ app/models/coffeeshop.rb
@@ -1,3 +1,4 @@
 class Coffeeshop < ActiveRecord::Base
-    
+    belongs_to :location
+    has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
-    
+    belongs_to :user
+    belongs_to :coffeeshop
+    has_many :comments
 end
