a9766d93378cd966882d2a4cd17cfbc6bf00c905
Jonathan Dust <d24dust@hotmail.com>
2019-03-15 16:45:09 -0500

    Users can now signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     get "/login" do
         if !logged_in?
+            session[:errrors] = ""
             erb :"user/login"
         else
             redirect "/"
@@ -9,11 +10,14 @@ class UsersController < ApplicationController
     end
 
     get "/signup" do
-
+        if !logged_in?
+            erb :"user/new"
+        else
+            redirect "/"
+        end
     end
 
     post "/login" do
-
         if params[:email] == nil || params[:password] == nil
             session[:errors] = "login_blank"
             redirect "/login"
@@ -30,7 +34,29 @@ class UsersController < ApplicationController
     end
 
     post "/signup" do
+        @user = User.new(params[:user])
 
+        if new_location_requested?
+            @user.build_location(city: params[:location][:city], state: params[:location][:state])
+        end
+        
+        if @user.save
+            session[:user_id] = @user.id
+            redirect "/"
+        else
+            erb :"user/new"
+        end
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/login"
+    end
+
+    helpers do
+        def new_location_requested?
+            !params[:location][:city].empty?
+        end
     end
 
 end
