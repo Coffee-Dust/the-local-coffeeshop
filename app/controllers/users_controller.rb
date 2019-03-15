22fb12ce8785d4f833330c87bb1ed74cdc072f53
Jonathan Dust <d24dust@hotmail.com>
2019-03-15 13:37:55 -0500

    add login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
     get "/login" do
-
+        if !logged_in?
+            erb :"user/login"
+        else
+            redirect "/"
+        end
     end
 
     get "/signup" do
@@ -10,6 +14,19 @@ class UsersController < ApplicationController
 
     post "/login" do
 
+        if params[:email] == nil || params[:password] == nil
+            session[:errors] = "login_blank"
+            redirect "/login"
+        end
+
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(password: params[:password])
+            session[:user_id] = @user.id
+            redirect "/"
+        else
+            session[:errors] = "login"
+            redirect "/login"
+        end
     end
 
     post "/signup" do
