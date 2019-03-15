c2df7aee44aa7d7290d3b4bf895d13c7255ed2cd
Jonathan Dust <d24dust@hotmail.com>
2019-03-14 20:13:39 -0500

    add popular_drinks feature to Coffeeshops

--- app/models/coffeeshop.rb
+++ app/models/coffeeshop.rb
@@ -1,4 +1,15 @@
 class Coffeeshop < ActiveRecord::Base
     belongs_to :location
     has_many :reviews
+
+    has_many :popular_drinks, class_name: "Drink"
+
+
+    def sort_popular_drinks
+        count = self.popular_drinks.group("name").count
+        names_in_order = count.sort_by{|key, value| value}.reverse.collect{|key,value|key}
+
+        return names_in_order
+    end
+
 end
