d7fc6d900b775e73a11b0244572b37a65d4ce742
Jonathan Dust <d24dust@hotmail.com>
2019-03-15 12:56:34 -0500

    create users_controller, add login/signup routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+    get "/login" do
+
+    end
+
+    get "/signup" do
+
+    end
+
+    post "/login" do
+
+    end
+
+    post "/signup" do
+
+    end
+
+end
