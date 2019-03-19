6da5a7a8724fbb6907b9648813387e9cd26c7e16
Jonathan Dust <d24dust@hotmail.com>
2019-03-19 15:16:57 -0400

    add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_172758) do
+ActiveRecord::Schema.define(version: 2019_03_19_190725) do
 
   create_table "coffeeshops", force: :cascade do |t|
     t.string "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_172758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "location_id"
+    t.boolean "admin"
   end
 
 end
