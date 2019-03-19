a7453d6f4959e399a5ddfcc90ef0f1c849dbbd67
Jonathan Dust <d24dust@hotmail.com>
2019-03-19 14:16:41 -0400

    add basic CRUD routes

--- app/controllers/coffeeshops_controller.rb
+++ app/controllers/coffeeshops_controller.rb
@@ -1,3 +1,27 @@
 class CoffeeshopsController < ApplicationController
-    
+    get "/coffeeshops" do
+        erb :"coffeeshop/index"
+    end
+
+    get "/coffeeshops/new" do
+        erb :"coffeeshop/new"
+    end
+
+    post "/coffeeshops" do
+
+    end
+
+    get "/coffeeshops/:id/:slug" do
+        @coffeeshop = Coffeeshop.find(params[:id])
+        erb :"coffeeshop/show"
+    end
+
+    get "/coffeeshops/:id/:slug/edit" do
+        erb :"coffeeshop/edit"
+    end
+
+    patch "/coffeeshops/:id/:slug" do
+        
+    end
+
 end
