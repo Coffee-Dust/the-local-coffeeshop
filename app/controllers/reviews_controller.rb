3caa19f492f654fce9d7b4c4c64344f86b5f2d7f
Jonathan Dust <d24dust@hotmail.com>
2019-03-22 16:40:28 -0400

    add routes for new reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,26 @@
 class ReviewsController < ApplicationController
-    
+    get "/reviews/new/:coffeeshop_id" do
+        if logged_in?
+            @coffeeshop = Coffeeshop.find(params[:coffeeshop_id])
+            erb :"reviews/new"
+        else
+            redirect :"/coffeeshops"
+        end
+    end
+
+    post "/reviews" do
+        review = Review.new(params[:review])
+        binding.pry
+        if params[:new_drink][:name].empty?
+            drink = Drink.new(params[:drink])
+        else
+            drink = Drink.new(name: params[:new_drink][:name], coffeeshop_id: params[:drink][:coffeeshop_id])
+        end
+        if review.save && drink.save
+            redirect "/coffeeshops/#{review.coffeeshop.id}/#{review.coffeeshop.name.to_slug}"
+        else
+            binding.pry
+            erb :"reviews/new"
+        end
+    end
 end
