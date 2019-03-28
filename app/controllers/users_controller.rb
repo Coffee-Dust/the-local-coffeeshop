8e0d3a4442d0ad9ca6a13269fb4528608816d713
Jonathan Dust <d24dust@hotmail.com>
2019-03-28 15:59:14 -0400

    add flash message functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,31 +22,33 @@ class UsersController < ApplicationController
     end
 
     post "/login" do
-        if params[:email] == nil || params[:password] == nil
-            session[:errors] = "login_blank"
-            redirect "/login"
-        end
-
+        
         @user = User.find_by(email: params[:email])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect "/"
         else
-            session[:errors] = "login"
-            redirect "/login"
+            flash[:message] = "Incorrect email/password"
+            erb :"user/login"
         end
     end
 
     post "/signup" do
-        @user = User.new(params[:user])
-        if new_location_requested?
-            @user.build_location(city: params[:location][:city], state: params[:location][:state])
-        end
+        if !User.find_by(email: params[:user][:email])
+            @user = User.new(params[:user])
+            if new_location_requested?
+                @user.build_location(city: params[:location][:city], state: params[:location][:state])
+            end
 
-        if @user.save
-            session[:user_id] = @user.id
-            redirect "/"
+            if @user.save
+                session[:user_id] = @user.id
+                redirect "/"
+            else
+                erb :"user/new"
+            end
         else
+            #User already exists
+            flash[:message] = "User already exists with that email."
             erb :"user/new"
         end
     end
