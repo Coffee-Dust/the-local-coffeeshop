a88f5d0b9f15d12b128017a90c20feab758ae9af
Jonathan Dust <d24dust@hotmail.com>
2019-03-28 16:37:16 -0400

    add index/show functionality

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,13 @@
 class LocationsController < ApplicationController
+
     get "/locations" do
+        @locations = Location.all
         erb :"location/index"
     end
+
+    get "/locations/:id/:slug" do
+        @location = Location.find(params[:id])
+        erb :"location/show"
+    end
+
 end
